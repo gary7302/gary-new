2a394d913b5a685943527c63dd0daf9e9ed605d9
gary7302 <gary7302@gmail.com>
2023-04-14 17:14:13 +0530

    sixth

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,3 +137,13 @@ STRIPE_SECRET_KEY = 'sk_test_51MrhFfSHZAJJXMruYFXUPbEM3Edu1WmjQ0BszczBCkfpqiZigN
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_ACCESS_KEY_ID='AKIA5A2JAV4RSIRERK6H'
+AWS_SECRET_ACCESS_KEY='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'
+AWS_STORAGE_BUCKET_NAME='backendcourse1'
+AWS_S3_SIGNATURE_NAME='s3v4'
+AWS_S3_REGION_NAME='ap-south-1'
+AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL=None
+AWS_S3_VERIFY=True
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
